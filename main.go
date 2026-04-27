813679dcfc8b08f924af6d64c0b8df5256b36ef3
Lucas Heber <lucas.heber07@gmail.com>
2026-04-27 20:00:05 -0300

    update main function to demonstrate array initialization and printing

--- main.go
+++ main.go
@@ -3,9 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	arr := [3]int{1, 2, 3}
-	fmt.Println(arr)
 
-	arr2 := [10]int{5: 400}
-	fmt.Println(arr2)
+	// for i := 0; i < 10; i++ {
+	// 	fmt.Println(i)
+	// }
+
+	// arr := [10]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+	// for _, elem := range arr {
+	// 	fmt.Println(elem)
+	// }
+
+	// versao 1.22
+	for range 10 {
+		fmt.Println("10")
+	}
 }
