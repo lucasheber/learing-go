4856125568dc48175fddb7f992a0ceb52357e7ee
Lucas Heber <lucas.heber07@gmail.com>
2026-04-27 19:31:44 -0300

    simplify main function and add array examples

--- main.go
+++ main.go
@@ -1,44 +1,11 @@
 package main
 
-import (
-	"fmt"
-	"learningGo/pacote"
-	variaveiseconstantes "learningGo/variaveis-e-constantes"
-)
+import "fmt"
 
 func main() {
-	fmt.Println("Hello World!")
+	arr := [3]int{1, 2, 3}
+	fmt.Println(arr)
 
-	fmt.Println(pacote.Foo)
-
-	a, b := swap(4, 5)
-	fmt.Println("Swap: ", a, b)
-
-	res, rem := dividir(10, 3)
-	fmt.Println("Dividir: ", res, rem)
-
-	fmt.Println("Higher Order: ", foo("Foo")("Bar"))
-
-	fmt.Print("====================== VARIAVEIS ======================\n")
-	variaveiseconstantes.Variaveis()
-}
-
-func somar(a, b int) int {
-	return a + b
-}
-
-func swap(a, b int) (int, int) {
-	return b, a
-}
-
-func dividir(a, b int) (res, rem int) {
-	res = a / b
-	rem = a % b
-	return
-}
-
-func foo(foo string) func(string) string {
-	return func(bar string) string {
-		return foo + " " + bar
-	}
+	arr2 := [10]int{5: 400}
+	fmt.Println(arr2)
 }
