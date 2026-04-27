a73218c6fe3f812599960346aa4dbd2900706d85
Lucas Heber <lucas.heber07@gmail.com>
2026-04-27 19:07:07 -0300

    initialize Go module and add main package with basic functionality

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"learningGo/pacote"
+)
+
+func main() {
+	fmt.Println("Hello World!")
+
+	fmt.Println(pacote.Foo)
+
+	a, b := swap(4, 5)
+	fmt.Println("Swap: ", a, b)
+
+	res, rem := dividir(10, 3)
+	fmt.Println("Dividir: ", res, rem)
+
+	fmt.Println("Higher Order: ", foo("Foo")("Bar"))
+}
+
+func somar(a, b int) int {
+	return a + b
+}
+
+func swap(a, b int) (int, int) {
+	return b, a
+}
+
+func dividir(a, b int) (res, rem int) {
+	res = a / b
+	rem = a % b
+	return
+}
+
+func foo(foo string) func(string) string {
+	return func(bar string) string {
+		return foo + " " + bar
+	}
+}
